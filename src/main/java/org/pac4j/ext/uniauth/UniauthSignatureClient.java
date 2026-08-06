f04acb6d0a458c74f773f4c950a22dfe6da44bb2
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 14:59:53 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/org/pac4j/ext/uniauth/UniauthSignatureClient.java
+++ src/main/java/org/pac4j/ext/uniauth/UniauthSignatureClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -22,7 +22,7 @@ import org.pac4j.core.profile.creator.ProfileCreator;
 
 /**
  * TODO
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class UniauthSignatureClient extends SignatureClient<SignatureCredentials, UniauthSignatureProfile, UniauthSignature> {
 
